1ccaa3dc2ab23fc515bfc5993638d236df498dc2
phuongtvt55 <phuongtvt55@gmail.com>
2024-10-08 21:11:44 +0700

    fix height

--- screens/register/components/CreateAccount.js
+++ screens/register/components/CreateAccount.js
@@ -166,7 +166,7 @@ const styles = StyleSheet.create({
     },
     logo: {
         width: 100,
-        height: 300,
+        height: 100,
         resizeMode: 'contain',
         alignSelf: 'center',
     },
